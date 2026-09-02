3c7aed398ae7b8505149197e2d7e61068bf7e5b2
Roy Perez <royjamesperezflores@gmail.com>
2026-09-02 12:32:41 -0500

    Add checkout_sessions and completed_sessions to session_metrics_daily

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,8 +1,10 @@
-CREATE TABLE session_metrics_daily (
-    date                         TEXT    NOT NULL,
-    dimension                    TEXT    NOT NULL,
-    dimension_value              TEXT    NOT NULL,
-    sessions                     INTEGER NOT NULL,
-    cart_add_sessions            INTEGER NOT NULL,
-    PRIMARY KEY (date, dimension, dimension_value)
+CREATE TABLE IF NOT EXISTS session_metrics_daily (
+  date TEXT NOT NULL,
+  dimension TEXT NOT NULL,
+  dimension_value TEXT NOT NULL,
+  sessions INTEGER NOT NULL,
+  cart_add_sessions INTEGER NOT NULL,
+  checkout_sessions INTEGER NOT NULL,
+  completed_sessions INTEGER NOT NULL,
+  PRIMARY KEY (date, dimension, dimension_value)
 );
